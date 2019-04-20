e7a24579a1c0b2eb8a3f69d4f86a6ac04da518c4
h-hiroki <gyouzakuuka@gmail.com>
2019-04-20 20:51:30 +0900

    fefactor

--- server.go
+++ server.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"github.com/labstack/echo"
-	"github.com/h-hiroki/echo_first/handler"
+	"github.com/h-hiroki/echo_first/handlers"
 )
 
 func main() {
 	e := echo.New()
 
 	e.File("/", "public/index.html")
-	e.GET("/todo", handler.GetTodos)
+	e.GET("/todo", handlers.GetTodos)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
