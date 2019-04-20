4275dcdaa09dcd590e54a2be894793858f6ad593
h-hiroki <gyouzakuuka@gmail.com>
2019-04-20 20:25:52 +0900

    ⛳️トップページのルーティング

--- server.go
+++ server.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"github.com/labstack/echo"
-	"net/http"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World!")
-	})
+
+	e.File("/", "public/index.html")
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
